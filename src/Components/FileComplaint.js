165ee9b47bae8dceb5f898c39ed9dc8efb738566
awwaab <awwaab2004@gmail.com>
2024-02-08 10:03:17 +0530

    changes

--- src/Components/FileComplaint.js
+++ src/Components/FileComplaint.js
@@ -1,30 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import ComplaintForm from '../complaintnew'
-
-  
+import React from 'react'
 
 const FileComplaint = () => {
-  const [items, setItems] = useState({});
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("mongodb://localhost:27017/"); // Replace the URL with your backend API endpoint
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
-        }
-        const data = await response.json();
-        setItems(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-  
-    fetchData();
-  }, []);
-  
-   
   return (
-    <div><ComplaintForm/></div>
+    <div>FileComplaint</div>
   )
 }
 
